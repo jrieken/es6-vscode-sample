1fcde75bb9521f6affe16f9ee1560bc0f87778a9
Johannes Rieken <johannes.rieken@gmail.com>
2015-07-02 16:41:12 +0200

    start server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 
 import {createServer} from 'http';
-import * as greets from 'greetings';
+import * as greets from './greetings';
 
 const headers = Object.freeze({ 'Content-Type': 'text/plain'});
 
-createServer((req, res) => {
+const server = createServer((req, res) => {
     res.writeHead(200, headers);
 
     let message = `${greets.n} ES6!\n`;
     res.end(message);
 });
+
+server.listen(8888);
